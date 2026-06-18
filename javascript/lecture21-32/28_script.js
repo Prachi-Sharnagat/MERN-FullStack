134bb6aeb919feaa9645631f5b252118511c5fec
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-06-18 21:42:22 +0530

    react js

---
+++ javascript/lecture21-32/28_script.js
@@ -0,0 +1,5 @@
+// what is iterator pattern ? 
+// generator functions ?
+// yield keyword?
+
+// iterator and generator brings The conceptt of iteration directly into the core language and provvide the mchanism to provide 
